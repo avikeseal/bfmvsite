b31fc943f35dc9b3b9898ddd26aa547d83b3ca4c
Avike Seal <avikeseal@gmail.com>
2025-04-11 14:40:00 -0400

    tweaked the js code for the logo beahvior on scroll

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ window.addEventListener("scroll", function(){
     nav.classList.toggle("sticky", this.window.scrollY > 0)
 });
 
-function removeElementOnScroll(elementId, scrollThreshold) {
+/*function removeElementOnScroll(elementId, scrollThreshold) {
     window.addEventListener("scroll", function() {
         if (this.window.scrollY > scrollThreshold) {
             const element = document.getElementById(elementId);
@@ -12,9 +12,25 @@ function removeElementOnScroll(elementId, scrollThreshold) {
                 // optionally remove the event listener after removing the element
                 window.removeEventListener("scroll", this);
             }
+            
         }
     });
 }
 
-removeElementOnScroll("logo", 300);
+removeElementOnScroll("logo", 100);*/
+
+function toggleElementOnScroll(elementId, scrollThreshold) {
+    window.addEventListener("scroll", function() {
+        const element = document.getElementById(elementId);
+        if (!element) return;
+
+        if (window.scrollY > scrollThreshold) {
+            element.style.display = "none";
+        } else {
+            element.style.display = "block";
+        }
+    });
+}
+
+toggleElementOnScroll("logo", 100);
 
