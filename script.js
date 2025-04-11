5b784e484885409609312136dfde67f35b4cd797
Avike Seal <avikeseal@gmail.com>
2025-04-11 14:15:39 -0400

    added js to make the logo disappear when scrolled down with nav fixed on top

--- script.js
+++ script.js
@@ -2,3 +2,19 @@ window.addEventListener("scroll", function(){
     var nav = document.querySelector("nav");
     nav.classList.toggle("sticky", this.window.scrollY > 0)
 });
+
+function removeElementOnScroll(elementId, scrollThreshold) {
+    window.addEventListener("scroll", function() {
+        if (this.window.scrollY > scrollThreshold) {
+            const element = document.getElementById(elementId);
+            if (element) {
+                element.remove();
+                // optionally remove the event listener after removing the element
+                window.removeEventListener("scroll", this);
+            }
+        }
+    });
+}
+
+removeElementOnScroll("logo", 300);
+
