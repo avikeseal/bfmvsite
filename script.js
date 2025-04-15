77d25954cdc64fd6a43dedc441cc43a23d3f422a
Avike Seal <avikeseal@gmail.com>
2025-04-15 12:58:14 -0400

    added some comments to script

--- script.js
+++ script.js
@@ -1,7 +1,4 @@
-window.addEventListener("scroll", function(){
-    var nav = document.querySelector("nav");
-    nav.classList.toggle("sticky", this.window.scrollY > 0)
-});
+
 
 /*function removeElementOnScroll(elementId, scrollThreshold) {
     window.addEventListener("scroll", function() {
@@ -55,19 +52,46 @@ toggleElementOnScroll("logo", 100);*/
 
 toggleElementOnScroll("logo", 10);*/
 
+//adds an event litener to the window object that listens for when the user 
+//scrolls:
+window.addEventListener("scroll", function(){
+    //grabs the first nav element on the page and stores it in the 
+    //nav variable 
+    var nav = document.querySelector("nav");
+    //toggles the sticky class on the nav element on the page
+    //if the page has been scrolled down from the top:
+    nav.classList.toggle("sticky", this.window.scrollY > 0)
+});
 
+//reusable function that accepts:
+//elementId: ID of the element to target
+//scrollThreshold: how many pixels the user ahs to scroll before changes happen
+//srollStyles: styles to apply after scrolling past threshold
+//topStyles: styles to apply when near the top
 function toggleElementOnScroll(elementId, scrollThreshold, scrollStyles, topStyles)
  {
+    //scroll listener inside this fucntion
+    //so the logic gets applied to the chosen element during scroll
     window.addEventListener("scroll", function () {
+        //gets the DOM element with the specific ID
         const element = document. getElementById(elementId);
+        //if the element doesn't exist on the page 
+        //stop the function to avoid errors
         if (!element) return;
 
+        //checks whether the page has been scrolled past the definite threshold
         const isScrolled = window.scrollY > scrollThreshold;
+        //if scrolled, use scrollStyles if not use top Styles
         const styles = isScrolled ? scrollStyles : topStyles;
+        //dynamically applies the selected styles to the element using 
+        //Object.assign which merges the style properties
         Object.assign(element.style, styles);
     });
  }
 
+ //applies the scroll effect w the element ID - logo
+ //after scrolling down 10px the logo fades out and hides
+ //when back to the top, the logo fades back in
  toggleElementOnScroll("logo", 10,
  {
     opacity: "0",
@@ -81,6 +105,9 @@ function toggleElementOnScroll(elementId, scrollThreshold, scrollStyles, topStyl
  }
 );
 
+//same s above but with nav-links
+//changes the margin and vertical position on scroll to create 
+//movement to being the links closer together or float them subtly
 toggleElementOnScroll("nav-links", 10,
     {
         margin: "1.5rem",
